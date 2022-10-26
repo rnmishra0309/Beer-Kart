ed87df466625e43b546de3a749d05afb2547ca63
rudra <rnmishra0309@gmail.com>
2022-10-26 11:10:18 +0530

    initial commit

---
+++ beerkart/src/main/java/com/beerkart/beerkart/BeerkartApplication.java
@@ -0,0 +1,13 @@
+package com.beerkart.beerkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerkartApplication.class, args);
+	}
+
+}
---
+++ beerkart/src/test/java/com/beerkart/beerkart/BeerkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerkart.beerkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
